711143d7d8883b2c1b9fec34c56adb40da6c0a4b
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-06-08 11:51:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate"){
+  return`This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun();
+saturdayFun('bathe the dog');
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(style="*") {
+  return function(special="special") {
+    return `You are ${style}${special}${style}!`
+  }
+}
+
+const Calculator = {
+  add: function() {
+    return 1 + 3;
+  },
+  subtract: function() {
+    return 1 - 3
+  },
+  multiply: function() {
+    return 1 * 3
+  },
+  divide: function(){
+    return 10 / 5
+  }
+}
+
+function actionApplyer(){
+  
+}
